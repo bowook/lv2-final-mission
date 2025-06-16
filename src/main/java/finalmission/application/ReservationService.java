1d0bc30060bf5ad964e474774d7e69dd7d12a8ab
bowook <mbosx316@naver.com>
2025-06-16 19:24:59 +0900

    refactor: ReservationService 개선

--- src/main/java/finalmission/application/ReservationService.java
+++ src/main/java/finalmission/application/ReservationService.java
@@ -1,14 +1,11 @@
 package finalmission.application;
 
+import finalmission.domain.lesson.entity.Lesson;
 import finalmission.domain.member.entity.Member;
-import finalmission.domain.member.exception.MemberNotFoundException;
-import finalmission.domain.member.repository.MemberRepository;
 import finalmission.domain.reservation.entity.Reservation;
-import finalmission.domain.reservation.exception.DuplicateReservationException;
 import finalmission.domain.reservation.exception.ReservationNotFoundException;
 import finalmission.domain.reservation.repository.ReservationRepository;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import finalmission.domain.time.entity.Time;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,70 +13,52 @@ import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class ReservationService {
 
-    private final ReservationRepository reservationRepository;
-    private final MemberRepository memberRepository;
+    private final ReservationRepository repository;
 
-    @Transactional
     public Reservation create(
-            final String name,
-            final String phoneNumber,
-            final String lesson,
-            final LocalDate date,
-            final LocalTime time) {
-        final Member member = memberRepository.findByNameAndPhoneNumber(name, phoneNumber);
-        final Reservation reservation = new Reservation(member, lesson, date, time);
+            final Member member,
+            final Lesson lesson,
+            final Time time
+    ) {
+        final Reservation reservation = new Reservation(member, lesson, time);
 
-        checkDuplicateReservation(lesson, date, time);
-
-        final Reservation savedReservation = reservationRepository.save(reservation);
-
-        return savedReservation;
+        return repository.save(reservation);
     }
 
-    @Transactional
-    public void deleteReservation(final Long id) {
-        reservationRepository.deleteById(id);
+    public void deleteReservation(final Member member, final Long id) {
+        repository.deleteByMemberAndId(member, id);
     }
 
-    @Transactional
-    public void updateReservation(final Reservation reservation, final LocalDate date, final LocalTime time) {
-        reservation.update(date, time);
+    public void updateDateAndTime(final Reservation reservation, final Time time) {
+        reservation.changeTime(time);
+    }
+
+    @Transactional(readOnly = true)
+    public boolean existsReservation(final Long lessonId) {
+        return repository.existsReservationByLessonId(lessonId);
     }
 
     @Transactional(readOnly = true)
     public List<Reservation> getCurrentSituations() {
-        return reservationRepository.findAll();
+        return repository.findAll();
     }
 
     @Transactional(readOnly = true)
-    public List<Reservation> getMyReservations(final Long id) {
-        final Member member = checkMemberPresence(id);
-
-        return reservationRepository.findByMember(member);
+    public List<Reservation> getMemberReservations(final Member member) {
+        return repository.findByMember(member);
     }
 
     @Transactional(readOnly = true)
     public Reservation findReservation(final Long id) {
-        final Reservation reservation = checkReservationPresence(id);
-
-        return reservation;
-    }
-
-    private Reservation checkReservationPresence(final Long id) {
-        return reservationRepository.findById(id)
+        return repository.findById(id)
                 .orElseThrow(() -> new ReservationNotFoundException("일치하는 예약이 존재하지 않습니다."));
     }
 
-    private Member checkMemberPresence(final Long id) {
-        return memberRepository.findById(id)
-                .orElseThrow(() -> new MemberNotFoundException("일치하는 회원이 존재하지 않습니다."));
-    }
-
-    private void checkDuplicateReservation(final String lesson, final LocalDate date, final LocalTime time) {
-        if (reservationRepository.existsByLessonAndDateAndTime(lesson, date, time)) {
-            throw new DuplicateReservationException("해당 수업에 대한 예약이 이미 존재합니다.");
-        }
+    @Transactional(readOnly = true)
+    public Reservation findReservationByMember(final Long id, final Member member) {
+        return repository.findReservationByIdAndMember(id, member);
     }
 }
