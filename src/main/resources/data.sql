b94211dc22a2a5e109ef5fe54566303a8a19013d
bowook <mbosx316@naver.com>
2025-06-16 19:13:44 +0900

    chore: yml 파일 및 data.sql 초기값 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,9 @@
-INSERT INTO MEMBER (name, phone_number)
-VALUES ('우가', '010-4874-3424');
+INSERT INTO MEMBER (email, password, name, phone_number)
+VALUES ('wooga@email.com', 'password', '우가', '010-4874-3424');
+
+INSERT INTO TIME (date, time)
+VALUES ('2025-06-12', '18:00'),
+       ('2025-06-13', '14:30');
+
+INSERT INTO LESSON (name)
+VALUES ('하체운동');
